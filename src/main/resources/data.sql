25edd3310679048c6cf2f08f73c1138c9e6a692a
dsoccer1980 <dsoccer1980@gmail.com>
2019-09-21 21:00:33 +0300

    Refactor data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,5 @@
-DELETE
-FROM fuel_type;
+DELETE FROM fuel_type;
 
-INSERT INTO fuel_type
-VALUES (1, '95');
-INSERT INTO fuel_type
-VALUES (2, '98');
-INSERT INTO fuel_type
-VALUES (3, 'D');
+INSERT INTO fuel_type VALUES (1, '95');
+INSERT INTO fuel_type VALUES (2, '98');
+INSERT INTO fuel_type VALUES (3, 'D');
