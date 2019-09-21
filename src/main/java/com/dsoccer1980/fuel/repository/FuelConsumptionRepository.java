1c690e3aff73cc2d731144fc79699f64eeeb8d17
dsoccer1980 <dsoccer1980@gmail.com>
2019-09-21 20:59:28 +0300

    Add queries with searching by driverId

--- src/main/java/com/dsoccer1980/fuel/repository/FuelConsumptionRepository.java
+++ src/main/java/com/dsoccer1980/fuel/repository/FuelConsumptionRepository.java
@@ -23,4 +23,14 @@ public interface FuelConsumptionRepository extends JpaRepository<FuelConsumption
     @Query("SELECT new com.dsoccer1980.fuel.domain.FuelConsumptionStatistic(MONTH(c.date), c.fuelType, sum(c.volume)," +
             " avg(c.price), sum(c.price*c.volume)) FROM FuelConsumption c GROUP BY MONTH(c.date)")
     List<FuelConsumptionStatistic> findFuelConsumptionGroupByFuelType();
+
+    @Query("SELECT new com.dsoccer1980.fuel.domain.MoneyByMonth(MONTH(c.date), sum(c.price*c.volume)) FROM FuelConsumption c WHERE c.driverId=:driverId GROUP BY MONTH(c.date)")
+    List<MoneyByMonth> sumSpentMoneyByDriverIdGroupByMonth(long driverId);
+
+    @Query("SELECT c FROM FuelConsumption c WHERE MONTH(c.date)=:month AND c.driverId=:driverId")
+    List<FuelConsumption> findFuelConsumptionByMonthAndByDriverId(int month, long driverId);
+
+    @Query("SELECT new com.dsoccer1980.fuel.domain.FuelConsumptionStatistic(MONTH(c.date), c.fuelType, sum(c.volume)," +
+            " avg(c.price), sum(c.price*c.volume)) FROM FuelConsumption c WHERE c.driverId=:driverId GROUP BY MONTH(c.date)")
+    List<FuelConsumptionStatistic> findFuelConsumptionByDriverIdGroupByFuelType(long driverId);
 }
