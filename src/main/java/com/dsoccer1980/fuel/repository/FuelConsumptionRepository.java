c47b86ac27e994b4883e721c03a0b12396544a2a
dsoccer1980 <dsoccer1980@gmail.com>
2019-09-24 11:44:57 +0300

    Add Transactional to repositories

--- src/main/java/com/dsoccer1980/fuel/repository/FuelConsumptionRepository.java
+++ src/main/java/com/dsoccer1980/fuel/repository/FuelConsumptionRepository.java
@@ -6,10 +6,12 @@ import com.dsoccer1980.fuel.domain.dto.MoneyByMonth;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public interface FuelConsumptionRepository extends JpaRepository<FuelConsumption, Long> {
 
     List<FuelConsumption> findByDriverId(long id);
--- src/main/java/com/dsoccer1980/fuel/repository/FuelTypeRepository.java
+++ src/main/java/com/dsoccer1980/fuel/repository/FuelTypeRepository.java
@@ -3,7 +3,9 @@ package com.dsoccer1980.fuel.repository;
 import com.dsoccer1980.fuel.domain.FuelType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface FuelTypeRepository extends JpaRepository<FuelType, Long> {
 }
